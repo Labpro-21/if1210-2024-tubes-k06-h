f2c08cce5e4751e9d9f8edd734739db79df3bbc2
Sabilul Huda <sabilul.huda2247@gmail.com>
2024-05-17 20:36:12 +0700

    revisi rng.py

--- src/rng.py
+++ src/rng.py
@@ -4,21 +4,16 @@ def lcg(x, a, c, m):
         yield x
 
 
-def random_uniform_sample(n, interval, seed=1):
+def random_uniform_sample(jml_num, batas, seed=1):
+    interval = [0, batas]
     a, c, m = 1103515245, 12345, 2 ** 31
     bsdrand = lcg(seed, a, c, m)
 
     lower, upper = interval[0], interval[1]
     sample = []
 
-    for i in range(n):
+    for i in range(jml_num):
         observation = (upper - lower) * (next(bsdrand) / (2 ** 31 - 1)) + lower
         sample.append(round(observation))
 
     return sample
-
-jmlnum = int(input(""))
-batas = int(input(""))
-
-rus = random_uniform_sample(jmlnum, [0, batas])
-print(rus)
