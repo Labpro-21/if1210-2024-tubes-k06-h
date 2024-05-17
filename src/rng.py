3889ec067ea5b12b5ccff4a204bd31476768e9a7
Sabilul Huda <sabilul.huda2247@gmail.com>
2024-05-17 21:01:59 +0700

    revisi rng.py

--- src/rng.py
+++ src/rng.py
@@ -1,10 +1,13 @@
+from datetime import datetime
+waktu_saat_ini = datetime.now()
+detik = (waktu_saat_ini - datetime(2024, 5, 17)).total_seconds()//1
 def lcg(x, a, c, m):
     while True:
         x = (a * x + c) % m
         yield x
 
 
-def random_uniform_sample(jml_num, batas, seed=1):
+def random_uniform_sample(jml_num, batas, seed=detik):
     interval = [0, batas]
     a, c, m = 1103515245, 12345, 2 ** 31
     bsdrand = lcg(seed, a, c, m)
