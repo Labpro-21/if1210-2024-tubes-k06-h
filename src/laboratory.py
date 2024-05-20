a9f79f56f0616427b2704eefaeeddbfed36e15f7
MAqmarFayyaz <167322519+MAqmarFayyaz@users.noreply.github.com>
2024-05-20 12:27:33 +0700

    Update laboratory.py

--- src/laboratory.py
+++ src/laboratory.py
@@ -1,25 +1,33 @@
-def monster_upgrade(monsters, idx, owca_coin):
-    pilihan_monster = monsters[idx]
+def cek(a, user_idx):
+    if a[0] == user_idx:
+            return True
+    return False
+
+def monster_upgrade(listmonster, monster,  idx, owca_coin):
+    pilihan_monster = listmonster[idx]
+    print(pilihan_monster)
     sudah_upgrade= False
     while not sudah_upgrade: 
-        if pilihan_monster['level'] >= 5:
+        if int(pilihan_monster[2]) >= 5:
             print ()
             print("Maaf, monster yang Anda pilih sudah memiliki level maksimum.")
             print ()
             break
         else:
             upgrade_harga_arr = [300, 500, 800, 1000] 
-            level_now = pilihan_monster['level']
+            level_now = int(pilihan_monster[2])
+            temp_owca = int(owca_coin)
             if level_now <= 4:
                 upgrade_harga = upgrade_harga_arr[level_now - 1]
-                print(f"\n{pilihan_monster['name']} akan di-upgrade ke level {pilihan_monster['level'] + 1}.")
-                print(f"Harga untuk melakukan upgrade {pilihan_monster['name']} adalah {upgrade_harga} OC.")
-                if owca_coin >= upgrade_harga:
+                print(f"\n {monster[int(pilihan_monster[1])][1]} akan di-upgrade ke level {int(pilihan_monster[2]) + 1}.")
+                print(f"Harga untuk melakukan upgrade {monster[int(pilihan_monster[1])][1]} adalah {upgrade_harga} OC.")
+                if temp_owca >= upgrade_harga:
                     confirm = input(">>> Lanjutkan upgrade (Y/N): ")
                     if confirm in ['Y', 'y']:
-                        pilihan_monster['level'] += 1
-                        owca_coin -= upgrade_harga
-                        print(f"Selamat, {pilihan_monster['name']} berhasil di-upgrade ke level {pilihan_monster['level']}!")
+                        pilihan_monster[2] = str(int(pilihan_monster[2]) + 1)
+                        # monster_inv[monster_idx][2] = pilihan_monster[2]
+                        temp_owca -= upgrade_harga
+                        print(f"Selamat, {monster[int(pilihan_monster[1])][1]} berhasil di-upgrade ke level {pilihan_monster[2]}!")
                         sudah_upgrade = True
                     elif confirm in ['N', 'n']:
                         print("Upgrade dibatalkan.")
@@ -32,18 +40,25 @@ def monster_upgrade(monsters, idx, owca_coin):
             else:
                 print("Maaf, monster sudah mencapai level maksimum.")
                 break
-    return owca_coin
+    return str(temp_owca)
 
-def laboratory(monsters, owca_coins):
+def laboratory(yourmonster , monster, owca_coins, user_idx, user):
     while True:  
         print ()
         print("\nSelamat datang di Lab Dokter Asep !!!\n")
         print ()
         print("============ MONSTER LIST ============")
-        for ix, monster in enumerate(monsters, 1):
-            print(f"{ix}. {monster['name']} (Level: {monster['level']})")
+        y = 0
+        mons_list = []
+        for i in yourmonster :
+            if cek(i, user_idx):
+                if i[1] != "monster_id" :
+                    mons_list.append(i)
+                    print(f"{y+1}. {monster[int(i[1])][1]} " ": level ", f"{i[2]}")
+                    y+=1
+                
 
-        print("\n============ UPGRADE PRICE ============")
+        print("\n============ UPGRADE PRICE =back===========")
         print("1. Level 1 -> Level 2: 300 OC")
         print("2. Level 2 -> Level 3: 500 OC")
         print("3. Level 3 -> Level 4: 800 OC")
@@ -57,14 +72,15 @@ def laboratory(monsters, owca_coins):
             break  
         elif pilih.isdigit():
             pilih = int(pilih)
-            if 1 <= pilih <= len(monsters):
-                owca_coins = monster_upgrade(monsters, pilih - 1, owca_coins)
+            if 1 <= pilih <= len(mons_list):
+                owca_coins = monster_upgrade(mons_list, monster, pilih-1, owca_coins)
+                user[int(user_idx)+1][4] = owca_coins
             else:
                 print("Pilihan tidak valid, silakan pilih nomor monster yang valid.")
         else:
             print("Pilihan tidak valid, silakan pilih nomor monster yang valid.")
 
-# CONTOH
+#das CONTOH
 if __name__ == "__main__":
     agent_inventory = [{'name': 'Chaca', 'level': 1}, 
                        {'name': 'Pikachow', 'level': 2}, 
@@ -72,5 +88,3 @@ if __name__ == "__main__":
     owca_coins = 1000
 
     laboratory(agent_inventory, owca_coins)
-
-
