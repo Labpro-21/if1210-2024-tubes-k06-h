66623e5d51eb9ff3cba751a1278323b4fd2e9dbe
MAqmarFayyaz <167322519+MAqmarFayyaz@users.noreply.github.com>
2024-05-20 12:30:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,66 @@ from menu_and_help import menu_help
 from laboratory import laboratory
 from laboratory import monster_upgrade
 from inventory import inventory
+from battle import battle
+from shop_currency import shop_currency
+# from shop_management import shop_management
+# from monster_management import monster_management
+from arena import arena
 
 # Inisialisasi seluruh variabel
 is_Logged_in = False
 is_Playing = True
 dict = load()
 user_idx = 1
+
+def mainhelp(idx, log_status) :
+    global is_Logged_in, user_idx
+
+    menu_help(dict['user'], idx, dict['user'][idx][1], log_status)
+    cmd_help = (int(input("\n>>> Pilih fungsi : ")))
+
+    if not log_status :
+        if cmd_help == 1 :
+            is_Logged_in, user_idx = login(dict['user'])
+
+        elif cmd_help == 2 :
+            register(dict['user'])
+            save(dict)
+
+    elif dict['user'][idx][3] == "agent" and log_status:
+        if cmd_help == 1 :
+            logout(dict['user'])
+            is_Logged_in = False
+            user_idx = 1
+
+        elif cmd_help == 2 :
+            inventory(dict['item_inventory'], dict['monster_inventory'], dict['monster'], dict['user'][user_idx][4], dict['user'][user_idx][0])
+
+        elif cmd_help == 3 :
+            laboratory(dict['monster_inventory'], dict['monster'], dict['user'][idx][4], dict['user'][user_idx][0], dict['user'])
+            save(dict)    
+
+        elif cmd_help == 4 :
+            battle(dict, user_idx-1)
+
+        elif cmd_help == 5 :
+            arena (dict, user_idx-1)
+
+        elif cmd_help == 6 :
+            shop_currency(dict, user_idx-1)
+#==================================================================================================
+    else : 
+    # Kalau role adalah admin
+        if cmd_help == 1 :
+            logout(dict['user'])
+            is_Logged_in = False
+            user_idx = 1
+
+        # elif cmd_help == 2 :
+        #     shop_management(dict['monster_inventory'], dict['item_inventory'], dict['monster_shop'], dict['item_shop'])
+
+        # elif cmd_help == 3 :
+        #     monster_management(dict)
 
 while is_Playing :
     if dict is not None :
@@ -29,53 +83,18 @@ while is_Playing :
             # Meminta pengguna untuk memberikan input berupa nilai 1/2/3 sesuai keterangan
             if command == 1 :
                 is_Logged_in, user_idx = login(dict['user']) 
+
             # Akan menjalankan fungsi login apabila pengguna memberikan input nilai 1
-            
+
             elif command == 2 :
-                register(dict['user'])
+                register(dict['user'], dict['item_inventory'], dict['monster_inventory'])
                 save(dict)
             # Akan menjalankan fungsi register apabila pengguna memberikan input nilai 2
             # Selanjutnya akan menjalankan fungsi save untuk menyimpan informasi hasil register
-              
+
             elif command == 3 :
-                menu_help(dict['user'], user_idx, dict['user'][user_idx][1], is_Logged_in)
-                cmd_help = (int(input("\n>>> Pilih fungsi : ")))
-                # Akan menjalankan fungsi menu_help apabila pengguna memberikan input nilai 3
-                if not is_Logged_in :
-                # Akan dieksekusi ketika belum login
-                    if cmd_help == 1 :
-                        is_Logged_in, user_idx = login(dict['user'])
-                    # Akan menjalankan fungsi login apabila pengguna memberikan input nilai 1
+                mainhelp(user_idx, is_Logged_in)
 
-                    elif cmd_help == 2 :
-                        register(dict['user'])
-                        save(dict)
-                    # Akan menjalankan fungsi register apabila pengguna memberikan input nilai 2
-                    # Selanjutnya akan menjalankan fungsi save untuk menyimpan informasi hasil register
-                
-                elif dict['user'][user_idx][3] == "agent" and is_Logged_in :
-                # Akan dieksekusi ketika sudah login dan memiliki role agent    
-                    if cmd_help == 1 :
-                        logout(dict['user'])
-                        is_Logged_in = False
-                        user_idx = 1
-                        
-                    elif cmd_help == 2 :
-                        inventory(dict['item_inventory'], dict['monster'], dict['monster_inventory'], dict['user'][4])
-
-                    elif cmd_help == 3 :
-                        laboratory(dict['monster'], dict['user'][4])
-  #==================================================================================================
-                else :
-                # Akan dieksekusi ketika role adalah admin
-                    if cmd_help == 1 :
-                        logout(dict['user'])
-                        is_Logged_in = False
-                        user_idx = 1
-                        
-                    elif cmd_help == 2 :
-                        print("\nLoading...")
-#==================================================================================================
             while command != 1 and command != 2 and command != 3 :
             # Akan melakukan pengulangan ketika command tidak tersedia dalam pilihan
                 print ("Command tidak tersedia.")
@@ -87,87 +106,26 @@ while is_Playing :
                     is_Logged_in, user_idx = login(dict['user']) 
 
                 elif command == 2 :
-                    register(dict['user'])
+                    register(dict['user'], dict['item_inventory'], dict['monster_inventory'])
                     save(dict)
 
                 elif command == 3 :
-                    menu_help(dict['user'], user_idx, dict['user'][user_idx][1], is_Logged_in)
-                    cmd_help = (int(input("\n>>> Pilih fungsi : ")))
+                    mainhelp(user_idx, is_Logged_in)
 
-                    if not is_Logged_in :
-                        if cmd_help == 1 :
-                            is_Logged_in, user_idx = login(dict['user'])
-
-                        elif cmd_help == 2 :
-                            register(dict['user'])
-                            save(dict)
-
-                    elif dict['user'][user_idx][3] == "agent" and is_Logged_in:
-                        if cmd_help == 1 :
-                            logout(dict['user'])
-                            is_Logged_in = False
-                            user_idx = 1
-
-                        # elif cmd_help == 2 :
-                        #     inventory()
-                        
-                        elif cmd_help == 3 :
-                            laboratory(dict['monster'], dict['user'][4])
-
-                    else : 
-                    # Kalau role adalah admin
-                        if cmd_help == 1 :
-                            logout(dict['user'], user_idx)
-                            is_Logged_in = False
-                            
-                        elif cmd_help == 2 :
-                            print("\nLoading...")
-            
             print("\nLoading...")
-        
+
         print("\nMasukkan command \"Help\" untuk daftar command yang dapat kamu panggil.")
         print("Masukkan command \"Exit\" untuk keluar dari program.")
-        
+
         cmd_main = input("\n >>> Masukkan command : ").lower()
         if cmd_main == "help" :
-            menu_help(dict['user'], user_idx, dict['user'][user_idx][1], is_Logged_in)
-            cmd_help = (int(input("\n>>> Pilih fungsi : ")))
+            mainhelp(user_idx, is_Logged_in)
 
-            if not is_Logged_in :
-                if cmd_help == 1 :
-                    is_Logged_in, user_idx = login(dict['user'])
-
-                elif cmd_help == 2 :
-                    register(dict['user'])
-                    save(dict)
-
-            elif dict['user'][user_idx][3] == "agent" and is_Logged_in:
-                if cmd_help == 1 :
-                    logout(dict['user'])
-                    is_Logged_in = False
-                    user_idx = 1
-
-                elif cmd_help == 2 :
-                    inventory(dict['item_inventory'], dict['monster'], dict['monster_inventory'], dict['user'][user_idx][4])
-
-                elif cmd_help == 3 :
-                    laboratory(dict['monster'], dict['user'][4])
-#==================================================================================================
-            else : 
-            # Kalau role adalah admin
-                if cmd_help == 1 :
-                    logout(dict['user'])
-                    is_Logged_in = False
-                    user_idx = 1
-                    
-                elif cmd_help == 2 :
-                    print("\nLoading...")
-#==================================================================================================
         elif cmd_main == "exit" :
             print ("\nTerima kasih!")
             print ("Sampai jumpa.")
             print ("\n")
-            exit('data')
+            exit(dict)
             is_Playing = False
 
         while cmd_main != "exit" and cmd_main != "help" :
@@ -178,39 +136,11 @@ while is_Playing :
             print("Masukkan command \"Exit\" untuk keluar dari program.")
             cmd_main = input("\n >>> Masukkan command : ").lower()
             if cmd_main == "help" :
-                menu_help(dict['user'], user_idx, dict['user'][user_idx][1], is_Logged_in)
-                cmd_help = (int(input("\n>>> Pilih fungsi : ")))
-
-                if not is_Logged_in :
-                    if cmd_help == 1 :
-                        is_Logged_in, user_idx = login(dict['user'])
-
-                    elif cmd_help == 2 :
-                        register(dict['user'])
-                        save(dict)
-
-                elif dict['user'][user_idx][3] == "agent" and is_Logged_in:
-                    if cmd_help == 1 :
-                        logout(dict['user'])
-                        is_Logged_in = False
-                        user_idx = 1
-
-                    elif cmd_help == 3 :
-                        laboratory(dict['monster'], dict['user'][4])
-#==================================================================================================
-                else : 
-                # Kalau role adalah admin
-                    if cmd_help == 1 :
-                        logout(dict['user'])
-                        is_Logged_in = False
-                        user_idx = 1
-                        
-                    elif cmd_help == 2 :
-                        print("\nLoading...")
+                mainhelp(user_idx, is_Logged_in)
 #==================================================================================================
             elif cmd_main == "exit" :
                 print ("\nTerima kasih!")
                 print ("Sampai jumpa.")
                 print("\n")
-                exit('data')
+                exit(dict)
                 is_Playing = False
